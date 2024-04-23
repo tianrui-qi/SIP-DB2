41d836e6f5ad9063dc2a0b2317a7a4443999f0fb
Tianrui Qi <tianrui.qi@icloud.com>
2024-04-23 15:25:40 -0400

    implementation for 2024-04-23-Group

--- selector.py
+++ selector.py
@@ -6,6 +6,7 @@ import pandas as pd
 import os
 import tqdm
 import matplotlib.pyplot as plt
+from typing import Literal
 
 
 class Selector:
@@ -25,7 +26,7 @@ class Selector:
         # if embd_path already in profile, skip
         if embd_path in self.profile["embd_path"].values: return
         # if not, add to end of profile
-        x1index = len(self)
+        x1index = len(self.profile)
         self.profile.loc[x1index, "embd_path"] = embd_path
         self.profile.loc[x1index, "feature_path"] = os.path.join(
             self.feature_fold, f"{x1index:04}.npy"
@@ -33,8 +34,8 @@ class Selector:
 
         x1embd, x1pos = Selector.getEmbd(embd_path, pval_thresh)
         for x2index in tqdm.tqdm(
-            range(len(self)), desc=f"{x1index:04}", 
-            dynamic_ncols=True, unit="run",
+            range(len(self.profile)), desc=f"{x1index:04}", 
+            dynamic_ncols=True, unit="run", leave=False,
         ):
             x2embd, x2pos = Selector.getEmbd(
                 self.profile.loc[x2index, "embd_path"], pval_thresh
@@ -74,32 +75,53 @@ class Selector:
         upper = int(np.ceil(feature[0, 1]))
         lower = int(np.floor(feature[-1, 1]))
 
-        plt.hist(feature[:, 1], bins=10*(upper-lower))
+        # 10%
+        #upper_10 = feature[int(len(feature)*0.1), 1]
+        #plt.axvline(upper_10, color="black", linestyle="--")
+        #plt.text(upper_10+0.1, plt.ylim()[1]*3200, "10%")
+        # 50%
+        #upper_50 = feature[int(len(feature)*0.5), 1]
+        #plt.axvline(upper_50, color="black", linestyle="--")
+        #plt.text(upper_50+0.1, plt.ylim()[1]*3200, "50%")
+
+        plt.hist(feature[:, 1], bins=10*(upper-lower), color="salmon")
         plt.xlim(lower, upper)
         plt.xlabel("Euclidean Distance")
-        plt.ylabel("Number of Feature (Pos)")
+        plt.ylabel("Number of Features/Positions")
         plt.show()
 
     def apply(
         self, embd_path: str, pval_thresh: float = None, 
-        k_read: float = 1.0, pos_range = 0,
+        top_k: float = 0.1, pos_range = 32, mod: Literal["all", "sep"] = "sep",
     ) -> np.ndarray:
-        # get top k_read of feature
-        feature = self[:]
-        if k_read <= 1: k_read = int(k_read * len(feature))     # percentage
-        feature = feature[:k_read, 0]
+        if mod not in ["all", "sep"]:
+            raise ValueError("mod must be 'all' or 'sep'")
+        elif mod == "all":
+            # get top_k features from all run
+            feature = self[:]
+            if top_k <= 1: top_k = int(top_k * len(feature))     # percentage
+            feature = feature[:top_k, 0]
+        elif mod == "sep":
+            # get top_k features for each run and combine
+            feature = np.zeros([0])
+            for i in range(len(self.profile)):
+                feature_i = self[i]
+                if top_k <= 1: top_k = int(top_k * len(feature_i))
+                feature = np.concatenate([feature, feature_i[:top_k, 0]])
+            # drop duplcates
+            feature = np.unique(feature)
         # algorithm below assume feature is sorted by pos from small to large
         feature = np.sort(feature)
 
         embd, pos = Selector.getEmbd(embd_path, pval_thresh)
         embd_selected = np.full((len(feature), 768), np.nan)
-        #embd_selected = np.zeros([len(feature), 768])   # [len(feature), 768]
         e = 0   # index for embd
         for p in range(len(feature)):
             # move e to the first read that is larger than feature[p]-pos_range
             # note that both pos and feature are sorted, so for next p, we can 
             # directly start from previous e
             while e < len(embd) and pos[e] < feature[p]-pos_range: e += 1
+            if e >= len(embd): break
             # if first read that larger than feature[p]-pos_range is also larger 
             # than feature[p]+pos_range, then we have no read in this range, set 
             # the embd of this pos to nan; 
@@ -113,6 +135,7 @@ class Selector:
                     distance = abs(pos[e_temp] - feature[p])
                     embd_selected[p] = embd[e_temp, :]
                 e_temp += 1
+            if e >= len(embd): break
         return embd_selected    # [len(feature), 768]
 
     @staticmethod
@@ -133,7 +156,7 @@ class Selector:
         if isinstance(index, slice):
             # concat selected features of all runs
             feature = np.zeros([0, 2])
-            for i in range(*index.indices(len(self))):
+            for i in range(*index.indices(len(self.profile))):
                 feature_path = self.profile.loc[i, "feature_path"]
                 if os.path.exists(feature_path): feature = np.concatenate(
                     [feature, np.load(feature_path)], axis=0
@@ -144,13 +167,15 @@ class Selector:
             # drop duplicates reads with same position, keep the first one
             _, index = np.unique(feature[:, 0], return_index=True)
             feature = feature[np.sort(index)]
-        if isinstance(index, int) and index >= 0 and index < len(self):
+        if isinstance(index, int) and index >= 0 and index < len(self.profile):
             feature_path = self.profile.loc[index, "feature_path"]
             if os.path.exists(feature_path): feature = np.load(feature_path)
+
         # TODO: now some of these features' pos are very close, which may cause
         # problem. Need to solve either by bucketing; with a parameter bucket 
         # width, choose the pos with largest distance in each bucket; each 
         # bucket shall also have a distance of bucket width to prevent overlap.
+
         return feature
 
     def __len__(self) -> int:
@@ -163,12 +188,19 @@ if __name__ == "__main__":
 
     data_fold = "data/stanford/"
     method = "pretrain"     # pretrain or finetune
-    chromosome = "6"
+    chr_list = [str(i) for i in range(1, 23)] + ["X"]   # BAM naming convention
     pval_thresh = 1e-5
-    
-    selector = Selector(feature_fold, ascending)
-    for run in os.listdir(os.path.join(data_fold, f"embd-{method}")):
-        selector.add(
-            os.path.join(data_fold, f"embd-{method}/{run}/{chromosome}.npy"), 
-            pval_thresh
-        )
+
+    for chromosome in tqdm.tqdm(
+        chr_list, 
+        dynamic_ncols=True, unit="chr", 
+    ):
+        selector = Selector(os.path.join(feature_fold, chromosome), ascending)
+        for run in tqdm.tqdm(
+            os.listdir(os.path.join(data_fold, f"embd-{method}")),
+            dynamic_ncols=True, unit="run", desc=chromosome, leave=False,
+        ):
+            selector.add(
+                os.path.join(data_fold, f"embd-{method}/{run}/{chromosome}.npy"), 
+                pval_thresh
+            )
