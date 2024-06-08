11fb07c6bdc01e90e989d1fe67baa0c6733956f3
Tianrui Qi <tianrui.qi@icloud.com>
2024-06-08 15:40:49 -0400

    implement getFeature

--- add.sh
+++ add.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=selector
-#SBATCH --output=/mnt/efs_v2/dbgap_tcga/users/tianrui.qi/SIP-DB2/data/out/selector_%A_%a.out
-#SBATCH --error=/mnt/efs_v2/dbgap_tcga/users/tianrui.qi/SIP-DB2/data/out/selector_%A_%a.err
+#SBATCH --output=/mnt/efs_v2/dbgap_tcga/users/tianrui.qi/SIP-DB2/data/out/%A_%a.out
+#SBATCH --error=/mnt/efs_v2/dbgap_tcga/users/tianrui.qi/SIP-DB2/data/err/%A_%a.err
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=24G
-#SBATCH --array=0-12
+#SBATCH --array=0-15
 
 # Activate your virtual environment if needed
 source /home/s.tianrui.qi/miniconda3/etc/profile.d/conda.sh
--- src/embd2repre.py
+++ src/embd2repre.py
@@ -5,8 +5,6 @@ import pandas as pd
 
 import os
 import tqdm
-import functools
-import multiprocessing
 
 
 __all__ = ["Selector"]
@@ -45,7 +43,7 @@ class Selector:
 
     def addFeature(
         self, embd_fold: str | list[str], chromosome: str,
-        hash_idx_start: int = 0, hash_idx_end: int = None,
+        hash_idx_start: int = 0, hash_idx_end: int = 249000,
     ) -> None:
         if isinstance(embd_fold, str): embd_fold = [embd_fold]
 
@@ -65,8 +63,7 @@ class Selector:
         # loop thought hash_index since we use same hash_size for store feature
         # and embd of all sample
         hash_idx_start = max(hash_idx_start, 0)
-        if hash_idx_end is None: hash_idx_end = self.hash_idx_max[chromosome]
-        else: hash_idx_end = min(hash_idx_end, self.hash_idx_max[chromosome])
+        hash_idx_end = min(hash_idx_end, self.hash_idx_max[chromosome])
         for hash_idx in tqdm.tqdm(
             range(hash_idx_start, hash_idx_end), 
             dynamic_ncols=True, smoothing=0, 
@@ -189,7 +186,7 @@ class Selector:
         x1: np.ndarray, x2: np.ndarray, batch_size: int = 10000
     ) -> tuple[np.ndarray, np.ndarray]:
         """
-        This function is equivalent to max of 0 of torch.cdist(x1, x2). 
+        This function is equivalent to max of axis 0 of torch.cdist(x1, x2). 
         We use some tricks to avoid memory overflow: use torch.cdist to compute 
         batch_size of x1 and x2 each time, and directly get the max distance of 
         axis 0 of the batch distance matrix instead of save matrix in the 
@@ -227,9 +224,7 @@ class Selector:
         x2batchnum = (x2.shape[0] + batch_size - 1) // batch_size
 
         x2dist = torch.full((len(x2),), float('-inf'))
-        for i in tqdm.tqdm(
-            range(x1batchnum), leave=False, unit="batch", dynamic_ncols=True
-        ):
+        for i in range(x1batchnum):
             start_i = i * batch_size
             end_i = min(start_i + batch_size, len(x1))
             x1_batch = x1[start_i:end_i]
@@ -247,6 +242,55 @@ class Selector:
         return x2dist.numpy()
 
     """ getFeature """
+
+    def getFeature(
+        self, chromosome: str | list[str] = None
+    ) -> dict[str, np.ndarray]:
+        # input check
+        if chromosome is None: 
+            chromosome = [str(i) for i in range(1, 23)] + ["X"]
+        if isinstance(chromosome, str): chromosome = [chromosome]
+
+        # get feature of each chromosome
+        for c in tqdm.tqdm(
+            chromosome, dynamic_ncols=True, smoothing=0,
+            unit="chromosome", desc="getFeature"
+        ):
+            # check if feature of current chromosome already get
+            feature_c_path = os.path.join(self.feature_fold, c, "feature.npy")
+            if os.path.exists(feature_c_path): continue
+            # get feature of current chromosome by loop through all hash_idx
+            feature_c = []
+            for hash_idx in tqdm.tqdm(
+                range(self.hash_idx_max[c]), 
+                dynamic_ncols=True, leave=False, smoothing=0, 
+                unit="hash", desc=c,
+            ):
+                # load feature
+                hash_fold = f"{hash_idx:06d}.npy"[:3]
+                hash_file = f"{hash_idx:06d}.npy"[3:]
+                feature_ch_path = os.path.join(
+                    self.feature_fold, c, hash_fold, hash_file
+                )
+                if not os.path.exists(feature_ch_path): continue
+                feature_ch = np.load(feature_ch_path)
+                # get max distance of each bucket
+                bucket_idx = feature_ch[:, 1]%self.hash_size//self.bucket_size
+                for b in np.unique(bucket_idx):
+                    feature_c.append([
+                        hash_idx, b, np.max(feature_ch[bucket_idx==b][:, 3])
+                    ])
+            # to ndarray and save
+            feature_c = np.array(feature_c, dtype=np.float32)
+            np.save(feature_c_path, feature_c)
+        
+        # { chromosome : (hash_idx, bucket_idx, distance) }
+        return {
+            c: np.load(os.path.join(self.feature_fold, c, "feature.npy"))
+            for c in chromosome
+        }
+
+    """ applyFeature """
 
     """ getRepresentation """
 
@@ -289,127 +333,8 @@ class Selector:
         denominator = np.sqrt(np.dot(x1var, x2var.T))           # (N1, N2)
         return numerator / denominator                          # (N1, N2)
 
+
 class Other:
-    def __init__(self, feature_fold: str) -> None:
-        self.feature_fold = feature_fold
-
-    """ getFeature """
-
-    def getFeature(
-        self, bin_size: int = 100, top_k: float = 0.1
-    ) -> dict[str, np.ndarray]:
-        # get feature for each chromosome and save in disk by calling
-        # self.getFeatureChr in multiprocessing
-        with multiprocessing.Pool() as pool:
-            for _ in tqdm.tqdm(
-                pool.imap_unordered(
-                    functools.partial(
-                        self.getFeatureChr, bin_size=bin_size, top_k=top_k
-                    ), [str(i) for i in range(1, 23)] + ["X"]
-                ),
-                total=23, dynamic_ncols=True, smoothing=0, 
-                unit="chromosome", desc="getFeature"
-            ): pass
-        # return all chromosome feature as dict by load from disk
-        return {
-            chromosome: self.getFeatureChrCached(chromosome, top_k, verbose=1) 
-            for chromosome in [str(i) for i in range(1, 23)] + ["X"]
-        }
-
-    def getFeatureChr(
-        self, chromosome: str, bin_size: int = 100, top_k: float = 0.1
-    ) -> np.ndarray:
-        # if feature already got before, return feature that only keep top_k 
-        # percentage of features
-        path = os.path.join(self.getFeature_fold, f"{chromosome}.npy")
-        if os.path.exists(path): 
-            return self.getFeatureChrCached(chromosome, top_k)
-
-        feature = np.zeros([0, 2])  # (position, distance)
-        for i in self.profile.index:
-            for j in self.profile.index:
-                # feature_ij, the distance of each position of sample_i by 
-                # distance matrix between sample_i and sample_j
-                # calculate in self.addFeature, sort by distance, already 
-                # removed duplicate position
-                feature_ij_path = os.path.join(
-                    self.addFeature_fold, f"{i:08}/{j:08}/{chromosome}.npy"
-                )
-                if not os.path.exists(feature_ij_path): continue
-                feature_ij = np.load(feature_ij_path)
-
-                # check if bucket size enough, extend if not
-                bins_new = int(max(feature_ij[:, 0]) // bin_size) + 1
-                bins_cur = len(feature)
-                if bins_new > bins_cur:
-                    extend = np.zeros((bins_new - bins_cur, 2))
-                    extend[:, 0] = (np.arange(bins_cur, bins_new)+0.5)*bin_size
-                    feature = np.concatenate([feature, extend])
-
-                # let the third column of feature_ij be the bucket index
-                feature_ij = np.column_stack([
-                    feature_ij, (feature_ij[:, 0] // bin_size)
-                ])
-                # remove duplicate bucket_index and keep the first one
-                # note that feature_ij sorted by distance, so by keep the first
-                # one, we keep the row with largest distance for each bucket
-                _, unique_index = np.unique(feature_ij[:, 2], return_index=True)
-                feature_ij = feature_ij[unique_index]
-                # for each feature, put it into corresponding bucket if that 
-                # feature's distance is larger than the distance store in bucket
-                feature_ij, bucket_index = feature_ij[:, 0:2], feature_ij[:, 2]
-                bucket_index = bucket_index.astype(int)
-                valid = feature_ij[:, 1] > feature[bucket_index, 1]
-                feature[bucket_index[valid]] = feature_ij[valid]
-
-        # cache feature for future use
-        if not os.path.exists(os.path.dirname(path)):
-            os.makedirs(os.path.dirname(path), exist_ok=True)
-        np.save(path, feature)
-
-        # return feature that only keep top_k percentage of features
-        return self.getFeatureChrCached(chromosome, top_k)
-
-    def getFeatureChrCached(
-        self, chromosome: str, top_k: float = 0.1, verbose: bool = False,
-    ) -> np.ndarray:
-        # load cached feature
-        path = os.path.join(self.getFeature_fold, f"{chromosome}.npy")
-        if not os.path.exists(path): raise FileNotFoundError(
-            f"Chromosome {chromosome} feature not found." + 
-            "Please run getFeature or getFeatureChr first."
-        )
-        feature = np.load(path)
-        feature_total = len(feature)    # record
-        # transfer top_k into number of features we want to keep
-        top_k = int(top_k*len(feature)) if top_k <= 1 else int(top_k)
-        # remove row in feature that distance is 0
-        feature = feature[feature[:, 1] != 0]
-        feature_nonzero = len(feature)  # record
-        # sort by distance
-        feature = feature[np.argsort(feature[:, 1])[::-1]]
-        feature_min = feature[0, 1]     # record
-        feature_max = feature[0, 1]     # record
-        # keep top_k percentage or number of features
-        feature = feature[:top_k]
-        # sort by position
-        feature = feature[np.argsort(feature[:, 0])]
-
-        if verbose: print(
-            "Chromosome {}\t".format(chromosome) +
-            f"min {feature_min:.2f} max {feature_max:.2f}\t" + 
-            f"total {feature_total:>{10},}\t" +
-            f"nonzero {feature_nonzero:>{10},}\t" +
-            f"selected {len(feature):>{10},}\t" + 
-            f"nonzero/total {feature_nonzero*100/feature_total:.2f}%\t" + 
-            f"selected/total {len(feature)*100/feature_total:.2f}%\t" +
-            f"selected/nonzero {len(feature)*100/feature_nonzero:.2f}%"
-        )
-
-        return feature  # [K, (position, distance)]
-
-    """ applyFeature """
-
     def applyFeature(self, embd_fold: str, pos_range: int = 25) -> np.ndarray:
         """
         TODO: parallelize chromosome
@@ -465,19 +390,3 @@ class Other:
             if e >= len(embd): break
 
         return embd_selected    # [Kc, 768]
-
-    """ help function """
-
-    @staticmethod
-    def getEmbd(
-        embd_fold: str, chromosome: str, pval_thresh: float = None
-    ) -> tuple[np.ndarray, np.ndarray]:
-        # read embd of given path
-        embd = np.load(os.path.join(embd_fold, f"{chromosome}.npy"))
-        # filter read that cover at least one variants with p-value<=pval_thresh
-        if pval_thresh is not None:
-            embd = embd[embd[:, 769-int(np.log10(pval_thresh))]>=1, :]
-        # sort row by position embd[:, 768]
-        embd = embd[embd[:, 768].argsort()]
-        # embd[:, :768] for embedding, embd[:, 768] for pos
-        return embd[:, :768], embd[:, 768].astype(np.int32)     # (N, 768), (N,)
